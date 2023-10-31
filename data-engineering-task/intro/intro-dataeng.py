49176c69adb166877f416ce6952b8f469f44a224
Anthony Cabulang <zyxanthony15@gmail.com>
2023-10-31 23:53:36 +0800

    First commit-adding subdirectory and adding file

---
+++ data-engineering-task/intro/intro-dataeng.py
@@ -0,0 +1,37 @@
+#Context: Get the Ratings of the Users in Datacamp and Put it in a Recommendation System
+
+
+#Querying the table
+# Complete the connection URI
+connection_uri = "postgresql://repl:password@localhost:5432/datacamp_application" 
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+# Get user with id 4387
+user1 = pd.read_sql("SELECT * FROM rating WHERE user_id=4387", db_engine)
+
+# Get user with id 18163
+user2 = pd.read_sql("SELECT * FROM rating WHERE user_id=18163", db_engine)
+
+# Get user with id 8770
+user3 = pd.read_sql("SELECT * FROM rating WHERE user_id=8770", db_engine)
+
+# Use the helper function to compare the 3 users
+print_user_comparison(user1, user2, user3)
+
+
+#Transform
+# Complete the transformation function
+def transform_avg_rating(rating_data):
+    # Group by course_id and extract average rating per course
+    avg_rating = rating_data.groupby('course_id').rating.mean()
+    # Return sorted average ratings per course
+    sort_rating = avg_rating.sort_values(ascending=False).reset_index()
+    return sort_rating
+
+# Extract the rating data into a DataFrame    
+rating_data = extract_rating_data(db_engines)
+
+# Use transform_avg_rating on the extracted data and print results
+avg_rating_data = transform_avg_rating(rating_data)
+print(avg_rating_data) 
+
