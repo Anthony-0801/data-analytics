cf61e69aa77f7d270e9a4518464ae8f0276a7662
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-04 20:25:18 +0800

    Completing the task

--- data-engineering-task/intro/intro-dataeng.py
+++ data-engineering-task/intro/intro-dataeng.py
@@ -35,3 +35,73 @@ rating_data = extract_rating_data(db_engines)
 avg_rating_data = transform_avg_rating(rating_data)
 print(avg_rating_data) 
 
+
+#Find missing values and transform the data
+course_data = extract_course_data(db_engines)
+
+# Print out the number of missing values per column
+print(course_data.isnull().sum())
+
+# The transformation should fill in the missing values
+def transform_fill_programming_language(course_data):
+    imputed = course_data.fillna({"programming_language": "R"})
+    return imputed
+
+transformed = transform_fill_programming_language(course_data)
+
+# Print out the number of missing values per column of transformed
+print(transformed.isnull().sum())
+
+
+# Complete the transformation function
+def transform_recommendations(avg_course_ratings, courses_to_recommend):
+    # Merge both DataFrames
+    merged = courses_to_recommend.merge(avg_course_ratings) 
+    # Sort values by rating and group by user_id
+    grouped = merged.sort_values("rating", ascending=False).groupby("user_id")
+    # Produce the top 3 values and sort by user_id
+    recommendations = grouped.head(3).sort_values("user_id").reset_index()
+    final_recommendations = recommendations[["user_id", "course_id","rating"]]
+    # Return final recommendations
+    return final_recommendations
+
+# Use the function with the predefined DataFrame objects
+recommendations = transform_recommendations(avg_course_ratings, courses_to_recommend)
+
+
+#Loading into the database
+connection_uri = "postgresql://repl:password@localhost:5432/dwh"
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+def load_to_dwh(recommendations):
+    recommendations.to_sql("recommendations", db_engine, if_exists ="replace")
+
+
+# Define the DAG so it runs on a daily basis
+dag = DAG(dag_id="recommendations",
+          schedule_interval="0 0 * * *")
+
+# Make sure `etl()` is called in the operator. Pass the correct kwargs.
+task_recommendations = PythonOperator(
+    task_id="recommendations_task",
+    python_callable=etl,
+    op_kwargs={"db_engines":  db_engines},
+)
+
+
+#Defining a function to share the recommendation engine
+def recommendations_for_user(user_id, threshold=4.5):
+    # Join with the courses table
+    query = """
+    SELECT title, rating FROM recommendations
+    INNER JOIN courses ON courses.course_id = recommendations.course_id
+    WHERE user_id=%(user_id)s AND rating>%(threshold)s
+    ORDER BY rating DESC
+    """
+    # Add the threshold parameter
+    predictions_df = pd.read_sql(query, db_engine, params = {"user_id": user_id, 
+                                                             "threshold": threshold})
+    return predictions_df.title.values
+
+# Try the function you created
+print(recommendations_for_user(12, 4.65))
